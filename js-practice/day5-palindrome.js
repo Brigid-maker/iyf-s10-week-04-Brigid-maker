90dc29a59800bf71d12d8a453e90d9609d0a4606
Brigid-maker <adhiambocheryl2018@gmail.com>
2026-04-18 23:33:02 +0300

    Complete Daily Challenges

---
+++ js-practice/day5-palindrome.js
@@ -0,0 +1,17 @@
+function isPalindrome(str) {
+    const cleaned = str.toLowerCase().replace(/
+        [^a-z0-9]/g,"");
+
+        let left = 0
+        let right = cleaned.length -1;
+
+        while (left < right) {
+            if (cleaned[left] !== cleaned[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true
+}
